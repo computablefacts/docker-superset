515b02983e418b36b62f8c9ead5aeeb5fbce3a4e
Patrick Brisacier <pbrisacier@mncc.fr>
2022-07-08 13:43:52 +0200

    Add configuration for alerts and reports. Add SMTP configuration

--- custom/pythonpath/superset_config_docker.py
+++ custom/pythonpath/superset_config_docker.py
@@ -63,3 +63,53 @@ LOGO_TOOLTIP = get_env_variable("LOGO_TOOLTIP", 'DataViz by ComputableFacts')
 LOGO_RIGHT_TEXT = get_env_variable("LOGO_RIGHT_TEXT", 'DataViz')
 
 FAVICONS = [{"href": get_env_variable("APP_FAVICON", '/static/assets/images/cf-logo.png')}]
+
+
+#--------------------------------------------------------------
+# Alert & Report
+# Changes to allow selenium to work with OIDC auth
+# See: https://github.com/apache/superset/issues/14330#issuecomment-885656343
+from superset.utils.urls import headless_url
+from superset.utils.machine_auth import MachineAuthProvider
+
+def auth_driver(driver, user):
+    # Setting cookies requires doing a request first, but /login is redirected to oauth provider, and stuck there.
+    driver.get(headless_url("/doesnotexist"))
+
+    cookies = MachineAuthProvider.get_auth_cookies(user)
+
+    for cookie_name, cookie_val in cookies.items():
+        driver.add_cookie(dict(name=cookie_name, value=cookie_val))
+
+    return driver
+
+# TODO: check if it still works when OIDC is disable
+WEBDRIVER_AUTH_FUNC = auth_driver
+#--------------------------------------------------------------
+# Alert & Report
+# Send email
+
+EMAIL_NOTIFICATIONS = get_env_variable("EMAIL_NOTIFICATIONS", 'False')
+
+if EMAIL_NOTIFICATIONS == 'True':
+
+    EMAIL_NOTIFICATIONS = True
+    EMAIL_REPORTS_SUBJECT_PREFIX = get_env_variable("EMAIL_REPORTS_SUBJECT_PREFIX", '[DataViz Report] ')
+
+    ALERT_REPORTS_NOTIFICATION_DRY_RUN = False
+
+
+    WEBDRIVER_BASEURL_USER_FRIENDLY = "http://localhost:8088/"
+
+    SMTP_HOST = get_env_variable("SMTP_HOST", 'smtp.sendgrid.net')
+    SMTP_STARTTLS = True
+    SMTP_SSL = False
+    SMTP_USER = get_env_variable("SMTP_USER", 'user')
+    SMTP_PORT = get_env_variable("SMTP_PORT", 587)
+    SMTP_PASSWORD = get_env_variable("SMTP_PASSWORD", 'change_it')
+    SMTP_MAIL_FROM = get_env_variable("SMTP_MAIL_FROM", 'superset@superset.com')
+
+# Deprecated ENABLE_SCHEDULED_EMAIL_REPORTS = True (WARNING:root:ENABLE_SCHEDULED_EMAIL_REPORTS is deprecated and will be removed in version 2.0.0)
+# Deprecated in 1.2.0: ENABLE_ALERTS, SCHEDULED_EMAIL_DEBUG_MODE, EMAIL_REPORTS_CRON_RESOLUTION, EMAIL_ASYNC_TIME_LIMIT_SEC, EMAIL_REPORT_BCC_ADDRESS, EMAIL_REPORTS_USER
+
+#--------------------------------------------------------------
