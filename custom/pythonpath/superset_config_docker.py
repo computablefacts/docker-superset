91847b5391ee79494905889da6f931009480fdf1
Patrick Brisacier <pbrisacier@mncc.fr>
2024-09-23 16:18:50 +0200

    Adapt super_config* to Superset v4

--- custom/pythonpath/superset_config_docker.py
+++ custom/pythonpath/superset_config_docker.py
@@ -1,19 +1,4 @@
 import os
-from typing import Optional
-
-def get_env_variable(var_name: str, default: Optional[str] = None) -> str:
-    """Get the environment variable or raise exception."""
-    try:
-        return os.environ[var_name]
-    except KeyError:
-        if default is not None:
-            return default
-        else:
-            error_msg = "The environment variable {} was missing, abort...".format(
-                var_name
-            )
-            raise EnvironmentError(error_msg)
-
 
 # The allowed translation for you app
 LANGUAGES = {
@@ -34,7 +19,7 @@ LANGUAGES = {
 
 ENABLE_PROXY_FIX = True
 
-SECRET_KEY = get_env_variable("SECRET_KEY", 'ChangeThisKeyPlease')
+SECRET_KEY = os.getenv("SECRET_KEY", 'ChangeThisKeyPlease')
 
 #---------------------------KEYCLOACK ----------------------------
 # See: https://github.com/apache/superset/discussions/13915
@@ -43,26 +28,26 @@ SECRET_KEY = get_env_variable("SECRET_KEY", 'ChangeThisKeyPlease')
 from keycloak_security_manager  import  OIDCSecurityManager
 from flask_appbuilder.security.manager import AUTH_OID
 
-OIDC_ENABLE = get_env_variable("OIDC_ENABLE", 'False')
+OIDC_ENABLE = os.getenv("OIDC_ENABLE", 'False')
 
 if OIDC_ENABLE == 'True':
     AUTH_TYPE = AUTH_OID
     CUSTOM_SECURITY_MANAGER = OIDCSecurityManager
     # See: https://flask-oidc.readthedocs.io/en/latest/#flask_oidc.OpenIDConnect.require_login
-    OIDC_CLIENT_SECRETS = get_env_variable("OIDC_CLIENT_SECRETS", '/app/pythonpath/client_secret.json')
+    OIDC_CLIENT_SECRETS = os.getenv("OIDC_CLIENT_SECRETS", '/app/pythonpath/client_secret.json')
     OIDC_COOKIE_SECURE = True
     OIDC_REQUIRE_VERIFIED_EMAIL = False
-    OIDC_OPENID_REALM = get_env_variable("OIDC_OPENID_REALM")
+    OIDC_OPENID_REALM = os.getenv("OIDC_OPENID_REALM")
     OIDC_INTROSPECTION_AUTH_METHOD = 'client_secret_post'
 #--------------------------------------------------------------
 
-APP_NAME = get_env_variable("APP_NAME", 'DataViz')
-APP_ICON = get_env_variable("APP_ICON", '/static/assets/images/cf-logo.png')
-APP_ICON_WIDTH = get_env_variable("APP_ICON_WIDTH", 26)
-LOGO_TOOLTIP = get_env_variable("LOGO_TOOLTIP", 'DataViz by ComputableFacts')
-LOGO_RIGHT_TEXT = get_env_variable("LOGO_RIGHT_TEXT", 'DataViz')
+APP_NAME = os.getenv("APP_NAME", 'DataViz')
+APP_ICON = os.getenv("APP_ICON", '/static/assets/images/cf-logo.png')
+APP_ICON_WIDTH = os.getenv("APP_ICON_WIDTH", 26)
+LOGO_TOOLTIP = os.getenv("LOGO_TOOLTIP", 'DataViz by ComputableFacts')
+LOGO_RIGHT_TEXT = os.getenv("LOGO_RIGHT_TEXT", 'DataViz')
 
-FAVICONS = [{"href": get_env_variable("APP_FAVICON", '/static/assets/images/cf-logo.png')}]
+FAVICONS = [{"href": os.getenv("APP_FAVICON", '/static/assets/images/cf-logo.png')}]
 
 
 #--------------------------------------------------------------
@@ -90,40 +75,40 @@ if OIDC_ENABLE == 'True':
 # Alert & Report
 # Send email
 
-EMAIL_NOTIFICATIONS = get_env_variable("EMAIL_NOTIFICATIONS", 'False')
+EMAIL_NOTIFICATIONS = os.getenv("EMAIL_NOTIFICATIONS", 'False')
 
 if EMAIL_NOTIFICATIONS == 'True':
 
     EMAIL_NOTIFICATIONS = True
-    EMAIL_REPORTS_SUBJECT_PREFIX = get_env_variable("EMAIL_REPORTS_SUBJECT_PREFIX", '')
+    EMAIL_REPORTS_SUBJECT_PREFIX = os.getenv("EMAIL_REPORTS_SUBJECT_PREFIX", '')
 
     ALERT_REPORTS_NOTIFICATION_DRY_RUN = False
 
 
-    WEBDRIVER_BASEURL_USER_FRIENDLY = get_env_variable("WEBDRIVER_BASEURL_USER_FRIENDLY", "http://localhost:8088/")
+    WEBDRIVER_BASEURL_USER_FRIENDLY = os.getenv("WEBDRIVER_BASEURL_USER_FRIENDLY", "http://localhost:8088/")
 
-    SMTP_HOST = get_env_variable("SMTP_HOST", 'smtp.sendgrid.net')
+    SMTP_HOST = os.getenv("SMTP_HOST", 'smtp.sendgrid.net')
     SMTP_STARTTLS = True
     SMTP_SSL = False
-    SMTP_USER = get_env_variable("SMTP_USER", 'user')
-    SMTP_PORT = get_env_variable("SMTP_PORT", 587)
-    SMTP_PASSWORD = get_env_variable("SMTP_PASSWORD", 'change_it')
-    SMTP_MAIL_FROM = get_env_variable("SMTP_MAIL_FROM", 'superset@superset.com')
+    SMTP_USER = os.getenv("SMTP_USER", 'user')
+    SMTP_PORT = os.getenv("SMTP_PORT", 587)
+    SMTP_PASSWORD = os.getenv("SMTP_PASSWORD", 'change_it')
+    SMTP_MAIL_FROM = os.getenv("SMTP_MAIL_FROM", 'superset@superset.com')
 
 # Deprecated ENABLE_SCHEDULED_EMAIL_REPORTS = True (WARNING:root:ENABLE_SCHEDULED_EMAIL_REPORTS is deprecated and will be removed in version 2.0.0)
 # Deprecated in 1.2.0: ENABLE_ALERTS, SCHEDULED_EMAIL_DEBUG_MODE, EMAIL_REPORTS_CRON_RESOLUTION, EMAIL_ASYNC_TIME_LIMIT_SEC, EMAIL_REPORT_BCC_ADDRESS, EMAIL_REPORTS_USER
 
 # Time before selenium times out after trying to locate an element on the page and wait
 # for that element to load for a screenshot.
-SCREENSHOT_LOCATE_WAIT = int(get_env_variable("SCREENSHOT_LOCATE_WAIT", 10))
+SCREENSHOT_LOCATE_WAIT = int(os.getenv("SCREENSHOT_LOCATE_WAIT", 10))
 # Time before selenium times out after waiting for all DOM class elements named
 # "loading" are gone.
-SCREENSHOT_LOAD_WAIT = int(get_env_variable("SCREENSHOT_LOAD_WAIT", 60))
+SCREENSHOT_LOAD_WAIT = int(os.getenv("SCREENSHOT_LOAD_WAIT", 60))
 # Selenium destroy retries
-SCREENSHOT_SELENIUM_RETRIES = int(get_env_variable("SCREENSHOT_SELENIUM_RETRIES", 5))
+SCREENSHOT_SELENIUM_RETRIES = int(os.getenv("SCREENSHOT_SELENIUM_RETRIES", 5))
 # Give selenium an headstart, in seconds
-SCREENSHOT_SELENIUM_HEADSTART = int(get_env_variable("SCREENSHOT_SELENIUM_HEADSTART", 3))
+SCREENSHOT_SELENIUM_HEADSTART = int(os.getenv("SCREENSHOT_SELENIUM_HEADSTART", 3))
 # Wait for the chart animation, in seconds
-SCREENSHOT_SELENIUM_ANIMATION_WAIT = int(get_env_variable("SCREENSHOT_SELENIUM_ANIMATION_WAIT", 5))
+SCREENSHOT_SELENIUM_ANIMATION_WAIT = int(os.getenv("SCREENSHOT_SELENIUM_ANIMATION_WAIT", 5))
 
 #--------------------------------------------------------------
--- docker/pythonpath/superset_config.py
+++ docker/pythonpath/superset_config.py
@@ -22,53 +22,42 @@
 #
 import logging
 import os
-from datetime import timedelta
-from typing import Optional
 
-from cachelib.file import FileSystemCache
 from celery.schedules import crontab
+#from flask_caching.backends.filesystemcache import FileSystemCache
 
 logger = logging.getLogger()
 
+DATABASE_DIALECT = os.getenv("DATABASE_DIALECT")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+DATABASE_DB = os.getenv("DATABASE_DB")
 
-def get_env_variable(var_name: str, default: Optional[str] = None) -> str:
-    """Get the environment variable or raise exception."""
-    logger.info(f"get_env_variable: read {var_name}")
-    try:
-        logger.info(f"get_env_variable: value {os.environ[var_name]}")
-        return os.environ[var_name]
-    except KeyError:
-        if default is not None:
-            logger.info(f"get_env_variable: value {default}")
-            return default
-        else:
-            error_msg = "The environment variable {} was missing, abort...".format(
-                var_name
-            )
-            raise EnvironmentError(error_msg)
-
-
-DATABASE_DIALECT = get_env_variable("DATABASE_DIALECT")
-DATABASE_USER = get_env_variable("DATABASE_USER")
-DATABASE_PASSWORD = get_env_variable("DATABASE_PASSWORD")
-DATABASE_HOST = get_env_variable("DATABASE_HOST")
-DATABASE_PORT = get_env_variable("DATABASE_PORT")
-DATABASE_DB = get_env_variable("DATABASE_DB")
+EXAMPLES_USER = os.getenv("EXAMPLES_USER")
+EXAMPLES_PASSWORD = os.getenv("EXAMPLES_PASSWORD")
+EXAMPLES_HOST = os.getenv("EXAMPLES_HOST")
+EXAMPLES_PORT = os.getenv("EXAMPLES_PORT")
+EXAMPLES_DB = os.getenv("EXAMPLES_DB")
 
 # The SQLAlchemy connection string.
-SQLALCHEMY_DATABASE_URI = "%s://%s:%s@%s:%s/%s" % (
-    DATABASE_DIALECT,
-    DATABASE_USER,
-    DATABASE_PASSWORD,
-    DATABASE_HOST,
-    DATABASE_PORT,
-    DATABASE_DB,
+SQLALCHEMY_DATABASE_URI = (
+    f"{DATABASE_DIALECT}://"
+    f"{DATABASE_USER}:{DATABASE_PASSWORD}@"
+    f"{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_DB}"
 )
 
-REDIS_HOST = get_env_variable("REDIS_HOST")
-REDIS_PORT = get_env_variable("REDIS_PORT")
-REDIS_CELERY_DB = get_env_variable("REDIS_CELERY_DB", "0")
-REDIS_RESULTS_DB = get_env_variable("REDIS_RESULTS_DB", "1")
+SQLALCHEMY_EXAMPLES_URI = (
+    f"{DATABASE_DIALECT}://"
+    f"{EXAMPLES_USER}:{EXAMPLES_PASSWORD}@"
+    f"{EXAMPLES_HOST}:{EXAMPLES_PORT}/{EXAMPLES_DB}"
+)
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = os.getenv("REDIS_PORT", "6379")
+REDIS_CELERY_DB = os.getenv("REDIS_CELERY_DB", "0")
+REDIS_RESULTS_DB = os.getenv("REDIS_RESULTS_DB", "1")
 
 ##### Change from original file
 #RESULTS_BACKEND = FileSystemCache("/app/superset_home/sqllab")
@@ -77,14 +66,29 @@ from cachelib.redis import RedisCache
 RESULTS_BACKEND = RedisCache(host=REDIS_HOST, port=REDIS_PORT, key_prefix='superset_results')
 ##### End of change
 
-class CeleryConfig(object):
-    BROKER_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_CELERY_DB}"
-    CELERY_IMPORTS = ("superset.sql_lab", "superset.tasks")
-    CELERY_RESULT_BACKEND = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_RESULTS_DB}"
-    CELERYD_LOG_LEVEL = "DEBUG"
-    CELERYD_PREFETCH_MULTIPLIER = 1
-    CELERY_ACKS_LATE = False
-    CELERYBEAT_SCHEDULE = {
+CACHE_CONFIG = {
+    "CACHE_TYPE": "RedisCache",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "CACHE_KEY_PREFIX": "superset_",
+    "CACHE_REDIS_HOST": REDIS_HOST,
+    "CACHE_REDIS_PORT": REDIS_PORT,
+    "CACHE_REDIS_DB": REDIS_RESULTS_DB,
+}
+DATA_CACHE_CONFIG = CACHE_CONFIG
+
+
+class CeleryConfig:
+    broker_url = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_CELERY_DB}"
+    imports = (
+        "superset.sql_lab",
+        "superset.tasks.scheduler",
+        "superset.tasks.thumbnails",
+        "superset.tasks.cache",
+    )
+    result_backend = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_RESULTS_DB}"
+    worker_prefetch_multiplier = 1
+    task_acks_late = False
+    beat_schedule = {
         "reports.scheduler": {
             "task": "reports.scheduler",
             "schedule": crontab(minute="*", hour="*"),
@@ -100,10 +104,9 @@ CELERY_CONFIG = CeleryConfig
 
 FEATURE_FLAGS = {"ALERT_REPORTS": True}
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
-WEBDRIVER_BASEURL = get_env_variable("WEBDRIVER_BASEURL", "http://superset:8088/")
+WEBDRIVER_BASEURL = os.getenv("WEBDRIVER_BASEURL", "http://superset:8088/") # When using docker compose baseurl should be http://superset_app:8088/
 # The base URL for the email report hyperlinks.
 WEBDRIVER_BASEURL_USER_FRIENDLY = WEBDRIVER_BASEURL
-
 SQLLAB_CTAS_NO_LIMIT = True
 
 #
